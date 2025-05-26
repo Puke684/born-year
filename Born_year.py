e0cd797af35c5cc5843be1391f3e2a76aaa0ce21
Puke684 <ilikeapples684@gmail.com>
2025-05-25 20:40:59 -0500

    Add files via upload

---
+++ Born_year.py
@@ -0,0 +1,9 @@
+year = input('What year is it again???\n>')
+age = input('How old are you? or How old are you turning this year?')
+
+year = int(year)
+age = int(age)
+
+born = year - age
+
+print(born)
